bc2a9f6e8dd86a54aaba68d026e75d57e53667a3
skowalczyk <skowalczyk@griddynamics.com>
2024-02-12 11:03:16 +0100

    first part of 2nd section

--- main.js
+++ main.js
@@ -40,3 +40,14 @@ slider.addEventListener("touchmove", dragging);
 
 document.addEventListener("mouseup", dragStop);
 document.addEventListener("touchend", dragStop);
+
+// House, Villa, Apartement choose
+const buttons = document.querySelector(".section-recommendations__type-buttons");
+buttons.addEventListener("click", (e) => {
+  const activeClass = "btn-transparent-primary--active";
+
+  if (e.target.localName === "button") {
+    Array.from(buttons.children).forEach((element) => element.classList.remove(activeClass));
+    e.target.classList.add(activeClass);
+  }
+});
